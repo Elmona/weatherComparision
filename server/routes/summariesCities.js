66cd44b4643093b08f86381c0d22ca355b643942
antonstrand <jag@antonstrand.se>
2019-01-09 22:38:05 +0100

    return temperature data

--- server/routes/summariesCities.js
+++ server/routes/summariesCities.js
@@ -1,8 +1,25 @@
 const router = require('express').Router()
 
+const mysql = require('../config/mysql')
+
+const getTempData = (startDate, endDate) => city =>
+  mysql.execute(`
+SELECT min(temperature) AS min,
+       avg(temperature) AS avg,
+       max(temperature) AS max
+FROM temperature
+INNER JOIN tempStation ON tempStation.city='${city}'
+AND tempStation.station = temperature.station
+WHERE temperature.timestamp BETWEEN '${startDate} 00:00:00' AND '${endDate} 00:00:00'
+`)
+
 router.route('/').post(({ body }, res) => {
-  console.log('post summary', body)
-  res.send(JSON.stringify({ message: 'success' }))
+  Promise.all(body.cities.map(getTempData(body.startDate, body.endDate)))
+    .then(citiesTemps => citiesTemps.map(([rows, _]) => rows))
+    .then(temps => {
+      console.log(temps)
+      res.send(JSON.stringify(temps))
+    })
 })
 
 module.exports = router
