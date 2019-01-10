e1b1902e3d7e34c9171bee8a2701326b553f7b73
antonstrand <jag@antonstrand.se>
2019-01-10 13:06:54 +0100

    refactor

--- server/routes/summariesCities.js
+++ server/routes/summariesCities.js
@@ -4,9 +4,9 @@ const mysql = require('../config/mysql')
 
 const getTempData = (startDate, endDate, city) =>
   mysql.execute(`
-SELECT min(temperature) AS min,
-       avg(temperature) AS avg,
-       max(temperature) AS max
+SELECT min(temperature) AS coldestDay,
+       avg(temperature) AS avgTemp,
+       max(temperature) AS warmestDay
 FROM temperature
 INNER JOIN tempStation ON tempStation.city='${city}'
 AND tempStation.station = temperature.station
@@ -15,34 +15,40 @@ WHERE temperature.timestamp BETWEEN '${startDate} 00:00:00' AND '${endDate} 00:0
 
 const getRainData = (startDate, endDate, city) =>
   mysql.execute(`
-SELECT min(amount) AS leastRain,
-     avg(amount) AS avgRain,
-     max(amount) AS mostRain
+SELECT sum(amount) AS totalRain,
+avg(amount) AS avgRain,
+max(amount) AS rainiestDay
 FROM rainReports
 INNER JOIN rainStation ON rainStation.city='${city}'
 AND rainStation.station = rainReports.station
+WHERE rainReports.timestamp BETWEEN '${startDate}' AND '${endDate}'
+`)
+
+const getCityInformation = city =>
+  mysql.execute(`
+SELECT informationText
+FROM city
+WHERE name='${city}'
 `)
 
 const getSummary = (startDate, endDate) => city =>
   Promise.all([
     getTempData(startDate, endDate, city),
-    getRainData(startDate, endDate, city)
+    getRainData(startDate, endDate, city),
+    getCityInformation(city)
   ])
-    .then(([temp, rain]) => ({
+    .then(([temp, rain, info]) => ({
       ...temp[0][0],
       ...rain[0][0],
+      ...info[0][0],
       city
     }))
     .catch(console.error)
 
-router.route('/').post(({ body }, res) => {
-  Promise.all(body.cities.map(getSummary(body.startDate, body.endDate)))
-    // .then(citiesTemps => citiesTemps.map(([rows, _]) => rows))
+const sendAsJSON = res => d => res.send(JSON.stringify(d))
 
-    .then(temps => {
-      console.log(temps)
-      res.send(JSON.stringify(temps))
-    })
+router.route('/').post(({ body: { cities, startDate, endDate } }, res) => {
+  Promise.all(cities.map(getSummary(startDate, endDate))).then(sendAsJSON(res))
 })
 
 module.exports = router
