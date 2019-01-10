a8a2ed66bed799d97fd4afde27d7bed5820ed036
antonstrand <jag@antonstrand.se>
2019-01-10 13:06:24 +0100

    Round to one decimal

--- client/src/View/CityPresentation.js
+++ client/src/View/CityPresentation.js
@@ -3,17 +3,43 @@ import React from 'react'
 import { Divider } from 'antd'
 import InfoRow from './InfoRow.js'
 
+const toOneDecimal = n => (n ? n.toFixed(1) : n)
+
 const CityPresentation = props => (
   <div>
     <h2>{props.city}</h2>
     <p style={{ textAlign: 'left' }}>{props.informationText}</p>
     <Divider />
-    <InfoRow label="Average temperature" value={props.avgTemp} type="℃" />
-    <InfoRow label="Warmest day" value={props.warmestDay} type="℃" />
-    <InfoRow label="Coldest day" value={props.coldestDay} type="℃" />
-    <InfoRow label="Average rainfall" value={props.avgRain} type=" mm" />
-    <InfoRow label="Rainiest day" value={props.rainiestDay} type=" mm" />
-    <InfoRow label="Total rainfall" value={props.totalRain} type=" mm" />
+    <InfoRow
+      label="Average temperature"
+      value={toOneDecimal(props.avgTemp)}
+      type="℃"
+    />
+    <InfoRow
+      label="Warmest day"
+      value={toOneDecimal(props.warmestDay)}
+      type="℃"
+    />
+    <InfoRow
+      label="Coldest day"
+      value={toOneDecimal(props.coldestDay)}
+      type="℃"
+    />
+    <InfoRow
+      label="Average rainfall"
+      value={toOneDecimal(props.avgRain)}
+      type=" mm"
+    />
+    <InfoRow
+      label="Rainiest day"
+      value={toOneDecimal(props.rainiestDay)}
+      type=" mm"
+    />
+    <InfoRow
+      label="Total rainfall"
+      value={toOneDecimal(props.totalRain)}
+      type=" mm"
+    />
   </div>
 )
 
