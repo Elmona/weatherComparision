02903545bb9cad7a9998a524ab759a55d793dcbb
antonstrand <jag@antonstrand.se>
2019-01-10 11:51:18 +0100

    Showing dummy-data

--- client/src/styles/Container.js
+++ client/src/styles/Container.js
@@ -1,13 +1,13 @@
-
 import styled from 'styled-components'
 
 const Container = styled.div`
-  position: fixed;
-  width: 100%;
-  height: 100%;
+  // position: fixed;
+  width: 100vw;
+  height: 100vh;
   left: 0;
   top: 0;
   background-color: #ebebeb;
+  scroll: auto;
 `
 
-export default Container 
+export default Container
