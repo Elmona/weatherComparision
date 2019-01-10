343a34c08dc0e2b45e2239b23d8fdd5ba80847a7
antonstrand <jag@antonstrand.se>
2019-01-10 10:00:54 +0100

    get both rain and temperature

--- server/routes/summariesCities.js
+++ server/routes/summariesCities.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const mysql = require('../config/mysql')
 
-const getTempData = (startDate, endDate) => city =>
+const getTempData = (startDate, endDate, city) =>
   mysql.execute(`
 SELECT min(temperature) AS min,
        avg(temperature) AS avg,
@@ -13,9 +13,32 @@ AND tempStation.station = temperature.station
 WHERE temperature.timestamp BETWEEN '${startDate} 00:00:00' AND '${endDate} 00:00:00'
 `)
 
+const getRainData = (startDate, endDate, city) =>
+  mysql.execute(`
+SELECT min(amount) AS leastRain,
+     avg(amount) AS avgRain,
+     max(amount) AS mostRain
+FROM rainReports
+INNER JOIN rainStation ON rainStation.city='${city}'
+AND rainStation.station = rainReports.station
+`)
+
+const getSummary = (startDate, endDate) => city =>
+  Promise.all([
+    getTempData(startDate, endDate, city),
+    getRainData(startDate, endDate, city)
+  ])
+    .then(([temp, rain]) => ({
+      ...temp[0][0],
+      ...rain[0][0],
+      city
+    }))
+    .catch(console.error)
+
 router.route('/').post(({ body }, res) => {
-  Promise.all(body.cities.map(getTempData(body.startDate, body.endDate)))
-    .then(citiesTemps => citiesTemps.map(([rows, _]) => rows))
+  Promise.all(body.cities.map(getSummary(body.startDate, body.endDate)))
+    // .then(citiesTemps => citiesTemps.map(([rows, _]) => rows))
+
     .then(temps => {
       console.log(temps)
       res.send(JSON.stringify(temps))
