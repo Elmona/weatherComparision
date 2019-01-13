6aeab028150b38519045fde8e0329fdc5277369f
antonstrand <jag@antonstrand.se>
2019-01-13 13:29:31 +0100

    Keeping correct date

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -3,14 +3,17 @@
 const mysql = require('mysql2')
 require('dotenv').config()
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 50,
-  queueLimit: 200
-}).promise()
+const connection = mysql
+  .createConnection({
+    host: 'localhost',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 50,
+    queueLimit: 200,
+    dateStrings: true
+  })
+  .promise()
 
 module.exports = connection
